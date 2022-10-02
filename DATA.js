f7eb582ee5d1e41b8480b89a560c844b7475a3a7
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-02 12:15:41 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "96ffbe2a-d15e-483f-a6bc-12f4f224e556",
+    "home_feed": [
+        {
+            "id": "08768e0b-ab22-4a75-b9df-ad546be3e232",
+            "artist_name": "poor",
+            "home_carousel": [
+                {
+                    "id": "f2f1557d-3b5d-4a27-b76e-b930725213a4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "d4758758-b22d-408b-9266-e2115e87b889",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png"
+                },
+                {
+                    "id": "97b9d94d-d6cd-4b60-880a-4c42cd588768",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/107.png"
+                },
+                {
+                    "id": "910dd24c-203a-4988-a930-24ff331a9118",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "5fc9ad0f-6bca-491c-a497-980c2416ef25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/251.png"
+                }
+            ],
+            "art_caption": "effect",
+            "art_description": "She serious test everything other sister. Sense although full. If state they site bad above right."
+        },
+        {
+            "id": "cb225848-97fc-46e1-a592-0d400091d1ae",
+            "artist_name": "let",
+            "home_carousel": [
+                {
+                    "id": "06fd3909-a470-43eb-97b9-d410e697bc09",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "da6c8b30-faa4-42f7-9669-8e5a0e5debd4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                },
+                {
+                    "id": "d445b459-a793-4815-b017-1572a084aab7",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/230.png"
+                },
+                {
+                    "id": "95dbcbd7-18b2-4f17-aa79-31d100f0bf5f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/77.png"
+                },
+                {
+                    "id": "1c27435d-5ae4-434d-9557-f766a1dec051",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/245.png"
+                }
+            ],
+            "art_caption": "main",
+            "art_description": "Beautiful movement late music. Land trouble manage unit population wide save direction. Land language dinner operation."
+        },
+        {
+            "id": "abd62d55-663e-4390-b34d-4e663a532a67",
+            "artist_name": "manager",
+            "home_carousel": [
+                {
+                    "id": "a690b56f-23fb-4761-a85a-88bf83ed2ab8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "9f18e08a-12d3-493e-95dc-9d6eba028a0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/215.png"
+                },
+                {
+                    "id": "6a1b0a22-83d6-4c3a-9b24-fc7ca585cd29",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/321.png"
+                },
+                {
+                    "id": "193b4434-990a-4e04-88a2-c868d0925a80",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/262.png"
+                },
+                {
+                    "id": "af571925-71c7-4216-bd96-272ef7c08418",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/368.png"
+                }
+            ],
+            "art_caption": "call",
+            "art_description": "Eye spend PM age. Adult culture scientist."
+        },
+        {
+            "id": "da500990-5456-42f5-bafe-1a8eb4c832de",
+            "artist_name": "fine",
+            "home_carousel": [
+                {
+                    "id": "1fce8b80-b4b3-4548-b55f-695767e90d3f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "ede51cc2-b178-453e-b9e2-6ee97d8c83b5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/199.png"
+                },
+                {
+                    "id": "1b5c39ac-14a8-4340-b658-4617fab5a9dd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/120.png"
+                },
+                {
+                    "id": "28b4e2b7-8e97-4a96-9be8-1aae24c0b444",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "8ae6ca76-bdbb-4eca-9b40-cec9c2a58296",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/150.png"
+                }
+            ],
+            "art_caption": "entire",
+            "art_description": "Station participant picture various let. Name child billion last difference none. Address particularly read choice trouble for management."
+        },
+        {
+            "id": "461a2b84-a667-4acd-be4f-6c6307a858a7",
+            "artist_name": "time",
+            "home_carousel": [
+                {
+                    "id": "e339bda7-4129-4337-a439-9e1b0be1cd2c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/270.png"
+                },
+                {
+                    "id": "748858ec-a242-482b-b8e0-658c6743961b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/213.png"
+                },
+                {
+                    "id": "fa44f78a-4ab0-4d33-95dc-f7cb572ca5f3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/144.png"
+                },
+                {
+                    "id": "ce035ecc-3191-4754-8de0-8e007a4d06a9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                },
+                {
+                    "id": "fe35d6be-cc79-4586-863e-358e5db7a135",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                }
+            ],
+            "art_caption": "because",
+            "art_description": "Second kid war all there poor right. Serve item performance value raise too. Everybody control sound begin present doctor commercial."
+        }
+    ]
+}
